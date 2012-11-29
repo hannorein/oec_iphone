c3367321f9127da5bfd98687f5ec41ebb2d05d69
Hanno Rein <hanno@hanno-rein.de>
2012-11-29 08:47:28 -0500

    before automatic update

---
+++ copy_images.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+commit (){
+  git add *
+  git commit -a -m "$1"
+}
+
+
+commit "before automatic update"
+cp open_exoplanet_catalogue/images/*.* data_images/
+commit "automatic update: copied images"
+
+#git push
