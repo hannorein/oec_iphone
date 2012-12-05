469c9e369e80e5cf5b29a8b1120507d06e695f23
Hanno Rein <hanno@hanno-rein.de>
2012-12-04 20:34:17 -0500

    HD 1641 image added

--- data_images/scripts/resizeOneImage.bash
+++ data_images/scripts/resizeOneImage.bash
@@ -2,7 +2,7 @@
 f=$1
 filename=${f%.*}
 echo $filename
-convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "$filename.png" "$filename.iPhone.png"
-convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "$filename.png" "$filename.iPad.png"
-convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "$filename.png" "$filename.web.png"
-convert -resize 800x600 -quality 90 "$filename.png" "$filename.weblarger.png"
+convert -resize x260 -resize '960x<' -resize 50% -gravity center -crop 480x130+0+0 +repage -quality 90 "$1" "$filename.iPhone.png"
+convert -resize x260 -resize '2048x<' -resize 50% -gravity center -crop 1024x130+0+0 +repage -quality 90 "$1" "$filename.iPad.png"
+convert -resize x400 -resize '1200x<' -resize 50% -gravity center -crop 600x200+0+0 +repage -quality 90 "$1" "$filename.web.png"
+convert -resize 800x600 -quality 90 "$1" "$filename.weblarger.png"
