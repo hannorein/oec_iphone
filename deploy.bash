63f5d68aeef2194245341e220809d3a048bc1c7f
Hanno Rein <hanno@hanno-rein.de>
2012-12-04 20:56:03 -0500

    updated deploy script to run php script

--- deploy.bash
+++ deploy.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
 rsync -rvz -e ssh --delete --exclude=".git/" --exclude="*/.git/" --chmod=uga+rw,Duga+rwx . hanno-rein.de:/home/hanno/public_html/exoplanet.hanno-rein.de/data/open_exoplanet_catalogue/
+ssh hanno-rein.de <<'ENDSSH'
+cd /home/hanno/public_html/exoplanet.hanno-rein.de/data
+echo "Starting update on"
+hostname
+php update.php
+echo "Update done."
+ENDSSH
