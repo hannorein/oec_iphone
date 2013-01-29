729cee3e7724b664a039065eb9437f0ad98cc214
Hanno Rein <hanno@hanno-rein.de>
2013-01-29 15:43:46 -0500

    Updated deploy script

--- deploy.bash
+++ deploy.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 rsync -rvz -e ssh --delete --exclude=".git/" --exclude="*/.git/" --chmod=uga+rw,Duga+rwx . hanno-rein.de:/home/hanno/public_html/exoplanet.hanno-rein.de/data/open_exoplanet_catalogue/
 ssh hanno-rein.de <<'ENDSSH'
-cd /home/hanno/public_html/exoplanet.hanno-rein.de/data
+cd /home/hanno/public_html/exoplanet.hanno-rein.de/open_exoplanet_catalogue
 echo "Starting update on"
 hostname
-php update.php
-php updateAddVideoLink.php
+git pull
 echo "Update done."
 ENDSSH
