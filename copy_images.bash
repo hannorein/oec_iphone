73e3a991b3c6ebee4910be2b18f12c7309d94feb
Hanno Rein <hanno@hanno-rein.de>
2013-05-03 20:43:16 -0400

    before automatic update

--- copy_images.bash
+++ copy_images.bash
@@ -7,7 +7,11 @@ commit (){
 
 
 commit "before automatic update"
-cp open_exoplanet_catalogue/images/*.* data_images/
+cp open_exoplanet_catalogue/images/*.* data_images/ > copiedimages.txt
+cat copiedimages.txt | while read line
+do
+  echo $line
+done
 commit "automatic update: copied images"
 
 #git push
