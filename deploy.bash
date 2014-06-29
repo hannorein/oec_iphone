805163031a5009523d01a8dbd7fefced68e2916c
Hanno Rein <hanno@hanno-rein.de>
2014-06-29 14:10:43 -0400

    before automatic update

--- deploy.bash
+++ deploy.bash
@@ -1,9 +1,18 @@
 #!/bin/bash
-rsync -rvz -e ssh --delete --exclude=".git/" --exclude="*/.git/" --chmod=uga+rw,Duga+rwx . hanno-rein.de:/home/hanno/public_html/exoplanet.hanno-rein.de/data/open_exoplanet_catalogue/
+#rsync -rvz -e ssh --delete --exclude=".git/" --exclude="*/.git/" --chmod=uga+rw,Duga+rwx . hanno-rein.de:/home/hanno/public_html/exoplanet.hanno-rein.de/data/open_exoplanet_catalogue/
 ssh hanno-rein.de <<'ENDSSH'
-cd /home/hanno/public_html/exoplanet.hanno-rein.de/open_exoplanet_catalogue
-echo "Starting update on"
-hostname
+cd /home/hanno/public_html/exoplanet.hanno-rein.de/
+
+pushd open_exoplanet_catalogue
 git pull
-echo "Update done."
+popd
+
+pushd oec
+git pull
+popd
+
+pushd oec_meta
+git pull
+popd
+
 ENDSSH
